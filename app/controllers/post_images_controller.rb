1162a63ba0e4c2b8f8a95e8ead39aa281534d2a2
EC2 Default User <ec2-user@ip-172-31-41-164.ap-northeast-1.compute.internal>
2024-04-17 01:14:01 +0000

    [Finish]4-15

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -16,13 +16,13 @@ class PostImagesController < ApplicationController
   end
 
   def show
-    @post_image =PostImage.find(params[:id])
+    @post_image = PostImage.find(params[:id])
   end
   
   def destroy
     post_image = PostImage.find(params[:id])
     post_image.destroy
-    redirect_to 'post_images_path'
+    redirect_to post_images_path
   end
   
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,14 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  # 投稿データのストロングパラメータ
+  private
+
+  def users
+    params.require(:user).permit(:name)
+
   end
 end
