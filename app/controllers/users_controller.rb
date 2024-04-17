f295e9d3f0cd3832a92b7b9354e21ca6a579124a
EC2 Default User <ec2-user@ip-172-31-41-164.ap-northeast-1.compute.internal>
2024-04-17 04:21:39 +0000

    [Finish]4-18

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
   # 投稿データのストロングパラメータ
   private
 
-  def users
-    params.require(:user).permit(:name)
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
 
   end
 end
